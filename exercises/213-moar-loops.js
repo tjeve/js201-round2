dec66ea8db5e0f5d71831977a84be0b8fa6c0dc9
Terrence Eve <tijayeve@gmail.com>
2020-03-06 00:01:14 -0600

    removeAnyWordWithZ complete

--- exercises/213-moar-loops.js
+++ exercises/213-moar-loops.js
@@ -28,7 +28,23 @@ removeZAnimals()
 // Write a function "removeAnyWordWithZ" that takes 1 argument: an array of strings
 // It should return a new array that has all of the items in the passed-in array minus
 // any words that contain the letter 'z' or 'Z' (case-insensitive)
+function removeAnyWordWithZ (array) {
 
+    let accumulator = []
+  
+    // 2) loop through "animals"
+  for (let idx = 0; idx < array.length; idx++) {
+    if (array[idx].toLowerCase().search('z') === -1) {
+      accumulator.push(array[idx])
+    }
+  }
+    // 3) add every item in "animals" to "animalsWithoutZ" unless the animal name
+    //    contains the letter "z"
+    //    HINT: remember you can search within a string
+  
+    // 4) return "animalsWithoutZ"
+  return accumulator
+  }
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
