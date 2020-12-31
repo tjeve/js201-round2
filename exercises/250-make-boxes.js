c1461044f55cca195e26e49f0aa9e540e7a94cc3
Terrence Eve <tijayeve@gmail.com>
2020-12-31 03:29:36 -0600

    completed makeBoxes

--- exercises/250-make-boxes.js
+++ exercises/250-make-boxes.js
@@ -8,35 +8,17 @@
 // *****
 // *****
 
-function makeSquare(size) {
-    let star = '*'
-    let newLine = '/n'
-    let line = []
-    let lineStr
-    let square = []
-    let squareStr
-    
-    for(let i = 0; i < size*size; i++) {
-      line.push(star)
-      if(line.length === size + 1) {
-        line.push('\n')
-        lineStr = line.join('')
-      }
-      
-    }
-    for(let j = 0; j < size; j++) {
-        square.push(lineStr)
-        if(square.length === size) {
-          console.log(square.join(''))
-          let squareStr = square.join('')
-        }
-      }
-    
-    return squareStr
+function makeSquare(squareSize) {
+  let square = ''
+  let star = '*'
 
+  for (let i = 0; i < squareSize; i++) {
+      if (i < squareSize - 1) {
+          square += star.repeat(squareSize) + '\n'
+      }else square += star.repeat(squareSize)
+  }
+  return square
 }
-
-makeSquare(5)
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // Write a function "makeBox" which is given a width and height and returns a
@@ -48,8 +30,29 @@ makeSquare(5)
 // *    *
 // ******
 
-function makeBox() {
-
+function makeBox(width, height) {
+  let box = ''
+  let star = '*'
+  let space = ' '
+  let newLine = '\n'
+  
+  for(let i = 0; i < height; i++){
+    if(i === 0) { // If first Line
+        box += star.repeat(width)
+        console.log(box)
+        console.log('---------------------------------')
+      } else if (i === height - 1) { //If last line
+        box += newLine + star.repeat(width)
+        console.log(box)
+        console.log('---------------------------------')
+      } else { //If any other line
+        box += newLine + star + space.repeat(width - 2) + star
+        console.log(box)
+        console.log('---------------------------------')
+      }
+      
+    }
+  return box
 }
 
 
@@ -63,6 +66,29 @@ function makeBox() {
 // * Welcome to DigitalCrafts *
 // ****************************
 
-function makeBanner() {
-
+function makeBanner(text) {
+  let width = text.length + 2
+  let banner = ''
+  let star = '*'
+  let space = ' '
+  let newLine = '\n'
+  
+  for(let i = 0; i < 3; i++){
+    if(i === 0) { // If first Line
+        banner += star.repeat(width + 2)
+        console.log(banner)
+        console.log('---------------------------------')
+      } else if (i === 2) { //If last line
+          banner += newLine + star.repeat(width + 2)
+          console.log(banner)
+          console.log('---------------------------------')
+      } else { //If any other line
+          banner += newLine + star + space + text + space + star
+          console.log(banner)
+          console.log('---------------------------------')
+        }
+      
+    }
+    
+  return banner
 }
